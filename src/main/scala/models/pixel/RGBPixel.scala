87037bf6052cd02bf4fd7999485ef4cae63dafec
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2023-12-14 18:08:22 +0100

    Implemented import from file. Added pixel grid class

--- src/main/scala/models/pixel/RGBPixel.scala
+++ src/main/scala/models/pixel/RGBPixel.scala
@@ -1,5 +1,13 @@
 package models.pixel
 
-case class RGBPixel() extends Pixel {
-  override def valid: Boolean = ???
+/**
+ * The RGBPixel case class represents a pixel in an image with RGB color values.
+ *
+ * @param red   The red color component of the pixel (0-255).
+ * @param green The green color component of the pixel (0-255).
+ * @param blue  The blue color component of the pixel (0-255).
+ */
+case class RGBPixel(red: Int, green: Int, blue: Int) extends Pixel {
+  override def valid: Boolean =
+    red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255
 }
