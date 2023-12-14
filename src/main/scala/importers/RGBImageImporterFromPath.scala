095c60475aed6c38f7a66409fe3022b5e0564f18
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2023-12-14 18:08:22 +0100

    Implemented import from file. Added pixel grid class

--- src/main/scala/importers/RGBImageImporterFromPath.scala
+++ src/main/scala/importers/RGBImageImporterFromPath.scala
@@ -1,8 +1,61 @@
 package importers
 
-import models.image.Image
-import models.pixel.RGBPixel
+import models.image.{Image, RGBImage}
+import models.pixel.{PixelGrid, RGBPixel}
 
-class RGBImageImporterFromPath extends RGBImageImporter {
-  override def importData(): Image[RGBPixel] = ???
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+/**
+ *  RGBImageImporterFromPath specializes in importing RGB images from a specified file path.
+ *
+ *  @param path The path to the file from which the RGB image is imported.
+ * */
+class RGBImageImporterFromPath(val path: String) extends RGBImageImporter {
+
+  /**
+   * Imports RGB image data from the specified file path.
+   *
+   * @return The image object containing RGBPixel data representing the imported image.
+   * */
+  override def importData(): Image[RGBPixel] = {
+    val image = readFile(new File(path))
+
+    val pixels = readPixels(image)
+
+    val pixelGrid = PixelGrid[RGBPixel](pixels)
+
+    RGBImage(pixelGrid)
+  }
+
+  /**
+   * Reads an image file from the specified File.
+   *
+   * @param file The File object representing the image file.
+   * @return A BufferedImage containing the image data.
+   */
+  private def readFile(file: File): BufferedImage =
+    ImageIO.read(file)
+
+  /**
+   * Reads RGB pixels from a BufferedImage.
+   *
+   * @param image The BufferedImage from which to read RGB pixels.
+   * @return A 2D array of RGBPixel representing the RGB values of the image.
+   */
+  private def readPixels(image: BufferedImage): Array[Array[RGBPixel]] = {
+    val height = image.getHeight
+    val width = image.getWidth
+    val pixels = Array.ofDim[RGBPixel](height, width)
+
+    for (y <- 0 until height)
+      for (x <- 0 until width) {
+        val color = new Color(image.getRGB(x, y))
+        val rgbPixel = RGBPixel(color.getRed, color.getGreen, color.getBlue)
+        pixels(y)(x) = rgbPixel
+      }
+    pixels
+  }
 }
--- src/main/scala/models/image/RGBImage.scala
+++ src/main/scala/models/image/RGBImage.scala
@@ -1,5 +1,10 @@
 package models.image
 
-import models.pixel.RGBPixel
+import models.pixel.{PixelGrid, RGBPixel}
 
-case class RGBImage() extends Image[RGBPixel]
+/**
+ * RGBImage is a class representing an image composed of RGB pixels.
+ *
+ * @param pixelGrid The PixelGrid contains the RGB pixels of the image.
+ * */
+case class RGBImage(pixelGrid: PixelGrid[RGBPixel]) extends Image[RGBPixel]
