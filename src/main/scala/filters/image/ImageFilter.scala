47aa404ffd3733e1a6eadbbd6bd61277dd703618
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2023-12-22 14:07:23 +0100

    final implementation of filters

--- src/main/scala/filters/image/ImageFilter.scala
+++ src/main/scala/filters/image/ImageFilter.scala
@@ -3,4 +3,10 @@ package filters.image
 import filters.Filter
 import models.image.Image
 
+/**
+ * A trait representing a generic image filter.
+ *
+ * @tparam P The type of the input image (e.g., GrayscaleImage, RGBImage).
+ * @tparam T The type of the output image (e.g., GrayscaleImage, RGBImage).
+ */
 trait ImageFilter[P <: Image[_], T <: Image[_]] extends Filter[P, T] {}
